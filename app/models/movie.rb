b6b570717110c2771f85647ddad37df040baa386
Shanshan Gong <sg3445@columbia.edu>
2023-10-06 19:42:58 +0000

    changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,44 +1,5 @@
 class Movie < ActiveRecord::Base
 
-
-  # def self.all_ratings
-  #   return ['G', 'PG', 'PG-13', 'R']
-  # end
-
-  # def self.with_ratings(ratings_list, sort_by)
-  #   Movie.where(:rating => ratings_list).order(sort_by)
-  # end
-
-
-  # def self.all_ratings
-  #   ratings=[]
-  #   Movie.select(:rating).distinct.each do |m|
-  #     ratings << m.rating
-  #   end
-  #   ratings
-  # end
-
-  # def self.with_ratings(ratings_list,sort_key)
-  #   # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
-  #   #  movies with those ratings
-  #   # if ratings_list is nil, retrieve ALL movies
-  #   if ratings_list==nil or ratings_list==[]
-  #     if sort_key==nil
-  #       Movie.all
-  #     else
-  #       Movie.order(sort_key)
-  #     end
-  #   else
-  #     ratings_list=ratings_list.map(&:upcase)
-  #     if sort_key==nil
-  #       Movie.where(rating:ratings_list)
-  #     else
-  #       Movie.where(rating:ratings_list).order(sort_key)
-  #     end
-  #   end
-  # end
-
-
   def self.all_ratings
     distinct_ratings = Movie.distinct.pluck(:rating)
     distinct_ratings.compact
